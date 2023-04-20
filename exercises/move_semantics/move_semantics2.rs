518c20e520f5c4246d683d532ee59a10fdf732a5
Gosuke Shibahara <goskays@gmail.com>
2023-04-19 21:58:04 -0700

    periodical

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -6,18 +6,37 @@
 
 fn main() {
     let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
-
-    // Do not change the following line!
+    let mut vec1 = fill_vec(vec0.clone());
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
-
     vec1.push(88);
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
+    let vec0 = Vec::new();
+    let mut vec1 = fill_vec(vec0.clone());
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+    vec1.push(88);
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+fn fill_vec2(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+fn fill_vec3(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(22);
--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,13 +2,14 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = vec![3; 100];
+    let a = "yyxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    // let a = ["hi"; 100];
 
     if a.len() >= 100 {
-        println!("Wow, that's a big array!");
+        println!("Wow, that's a big array!, {}", a.len());
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
